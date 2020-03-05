f8a5ed3311269a143682a92f7738f8380b0bbb50
mehrabkb <mehrabkordbacheh@yahoo.com>
2020-03-05 13:41:23 +0330

    add isAdmin function to check login user is admin or not

--- functions.php
+++ functions.php
@@ -1,8 +1,24 @@
 <?php 
+require('includes/dbconnect.inc.php');
 
 function islogin(){
+    // this function to check any user login 
     if (isset($_SESSION['username'])) {
         return true;
     }
     return false;
 }
+
+function isAdmin(){
+    // this funtcion to check login user is admin 
+    $username = $_SESSION['username'];
+    global $conn;
+    $stmt = $conn->prepare("SELECT * FROM `Users` WHERE `username` = '$username' ");
+    $stmt->execute();
+    $role = $stmt->fetchColumn(4);
+    if ($role == 1) {
+        return true;
+    }
+    return false;
+}
+
--- index.php
+++ index.php
@@ -2,28 +2,32 @@
 require('header.php');
 
 ?>
+<?php if (islogin()) { ?>
 
         <div class="container mt-4">
             <div class="row">
                 <div class="col-sm-4">
                     <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                        <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
-                        <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
-                        <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
-                        <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</a>
+                    <?php if (isAdmin()) {?>
+                           <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">shaba code</a>
+                    <?php } ?>
+                        <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">ajax</a>
                     </div>
                 </div>
                 <div class="col-sm-8">
                     <div class="tab-content" id="v-pills-tabContent">
+                    <?php if (isAdmin()) {?>
                         <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">...</div>
+                        <?php } ?>
                         <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
-                        <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
-                        <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
                     </div>
                 </div>
             </div>
         </div>
+                    <?php }else{?>
 
+        <p>for show option for you please login</p>
+                    <?php  } ?>
 <?php 
     require('footer.php');
 
