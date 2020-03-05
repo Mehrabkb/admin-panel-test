60d9e567eb9bc83535809f430d9d194dc7b28160
mehrabkb <mehrabkordbacheh@yahoo.com>
2020-03-05 11:32:07 +0330

    create directories and database connection file

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <title>Test</title>
+</head>
+<body>
+    <header>
+        <div class="container-fluid bg-light">
+            <div class="row m-0 p-2">
+                <div class="col-lg-2">
+                    Title Site
+                </div>
+                <div class="col-lg-8">
+                    <form class="form-inline" action="/action_page.php">
+                        <div class="form-group">
+                            <label for="username">username:</label>
+                            <input type="text" class="form-control" id="username" name="username">
+                        </div>
+                        <div class="form-group">
+                            <label for="pwd">Password:</label>
+                            <input type="password" class="form-control" id="pwd">
+                        </div>
+                        <button type="submit" class="btn btn-success">Login</button>
+                    </form>
+                </div>
+                <div class="col-lg-2">
+                    <a href="" class="btn btn-primary">Sign Up</a>
+                </div>
+            </div>
+        
+        </div>
+    </header>
